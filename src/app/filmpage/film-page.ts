2adcace047e454ed5555cb06723a5308cd310e34
Slava Olishevko <volishevko@xbsoftware.by>
2026-05-05 17:05:34 +0300

    feat: add FilmPage component

---
+++ src/app/filmpage/film-page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-film-page',
+  templateUrl: './film-page.html',
+  styleUrl: './film-page.css',
+})
+export class FilmPage {
+  constructor() {}
+}
