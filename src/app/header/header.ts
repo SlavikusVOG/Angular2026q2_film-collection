fe8c87ebda61ecb42c2ddfde1fffcb78856aba4d
Slava Olishevko <volishevko@xbsoftware.by>
2026-05-05 15:40:00 +0300

    feat: add header

---
+++ src/app/header/header.ts
@@ -0,0 +1,12 @@
+import { Component, inject } from "@angular/core";
+import { RouterLink} from "@angular/router";
+
+@Component({
+  selector: 'header',
+  templateUrl: './header.html',
+  styleUrl: 'header.css',
+  imports: [
+    RouterLink,
+  ],
+})
+export class Header {}
