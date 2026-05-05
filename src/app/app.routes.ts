631c51214a8f5781b1bafea335e5c2012d3ac32b
Slava Olishevko <volishevko@xbsoftware.by>
2026-05-05 15:51:50 +0300

    feat: update routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './homepage/homepage';
+import { AboutPage } from './about/about';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'homepage',
+    pathMatch: 'prefix',
+    component: HomePage,
+  },
+  {
+    path: 'about',
+    pathMatch: 'prefix',
+    component: AboutPage,
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'homepage',
+  },
+];
