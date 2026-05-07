a3aa0c2394fdc5ef48cc587ace51e48e69a256e0
Slava Olishevko <volishevko@xbsoftware.by>
2026-05-08 01:28:25 +0300

    feat: implement breadcrumbs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,39 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './homepage/homepage';
 import { AboutPage } from './about/about';
+import { FilmPage } from './film-page/film-page';
 
 export const routes: Routes = [
   {
     path: 'homepage',
     pathMatch: 'prefix',
     component: HomePage,
+    data: {
+      breadcrumb: {
+        homepage: 'Home',
+      },
+    }
   },
   {
     path: 'about',
     pathMatch: 'prefix',
     component: AboutPage,
+    data: {
+      breadcrumb: {
+        about: 'About'
+      },
+    }
+  },
+  {
+    path: 'homepage/film/:id',
+    pathMatch: 'prefix',
+    component: FilmPage,
+    data: {
+      breadcrumb: {
+        homepage: 'Home',
+        film: 'Film',
+      },
+    }
   },
   {
     path: '**',
--- src/app/breadcrumbs/breadcrumbs.ts
+++ src/app/breadcrumbs/breadcrumbs.ts
@@ -1,19 +1,34 @@
 import { Component, inject } from "@angular/core";
-import { ActivatedRoute, Router } from "@angular/router";
+import { Router, RouterLink, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-breadcrumbs',
   templateUrl: './breadcrumbs.html',
   styleUrl: 'breadcrumbs.css',
   imports: [
+    RouterLink,
   ]
 })
 export class Breadcrumbs {
-  private activeRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private activatedRoute = inject(ActivatedRoute);
 
   getCurrentRoute() {
-    console.log(this.activeRoute.snapshot);
-    // return this.activeRoute.snapshot.url.join('/');
-    return 'home'
+    const url = this.router.url;
+    if (url === '/') {
+      return [];
+    }
+    const segments = url.slice(1).split('/');
+    return segments;
+  }
+
+  getBreadcrumb(segment: string) {
+    // TODO: improve with active route
+    const config = this.router.config;
+    const route = config.find(route => route.path === segment);
+    if (route) {
+      return route.data?.['breadcrumb'][segment];
+    }
+    return null;
   }
 }
