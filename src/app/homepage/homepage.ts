f21cd86d79a4f298247b48944c1ccfedf03a1098
Slava Olishevko <volishevko@xbsoftware.by>
2026-05-09 16:45:26 +0300

    feat: implement search

--- src/app/homepage/homepage.ts
+++ src/app/homepage/homepage.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { HomepageService } from "./homepage.service";
 import { FilmCard } from "../film-card/film-card";
 import { Router } from "@angular/router";
@@ -15,12 +15,50 @@ import { Film } from "../models/film.data";
 export class HomePage {
   private homepageService = inject(HomepageService);
   private router = inject(Router);
+  private films = signal<Film[]>([]);
+
+  ngAfterViewInit() {
+    this.attachEvents();
+    this.focusSearch();
+    const allFilms = this.getAllFilms();
+    this.films.set(allFilms);
+  }
+
+  attachEvents() {
+    const searchInput = document.querySelector('search input') as HTMLInputElement;
+    if (searchInput) {
+      searchInput.addEventListener('input', this.onSearchInput.bind(this));
+    }
+  }
+
+  onSearchInput(event: Event) {
+    const searchInput = event.target as HTMLInputElement;
+    const searchValue = searchInput.value;
+    const allFilms = this.getAllFilms();
+    const filteredFilms = allFilms.filter(film => film.title.toLowerCase().includes(searchValue.toLowerCase()));
+    this.films.set(filteredFilms);
+  }
+
+  focusSearch() {
+    const searchInput = document.querySelector('search input') as HTMLInputElement;
+    if (searchInput) {
+      searchInput.focus();
+      searchInput.select();
+    }
+  }
 
   getFilms() {
+    return this.films();
+  }
+
+  getAllFilms() {
     return this.homepageService.getFilms();
   }
 
-  onFilmCardClick(film: Film) {
+  onFilmCardClick(event: Event, film: Film) {
+    if (event.target instanceof HTMLButtonElement || event.target instanceof SVGElement) {
+      return;
+    }
     this.router.navigate(['/homepage/film', film.id]);
   }
 }
